2f3464aac604557d30b4f1b1eb71b3b378b4209b
marinacharris <85807129+marinacharris@users.noreply.github.com>
2021-06-30 09:46:45 -0500

    Add files via upload

---
+++ ciclosForP65.java
@@ -0,0 +1,27 @@
+public class ciclosForP65 {
+    public static void main(String[] args) {
+        //for 
+        //conteo del 0 al 20
+        //for (inicio;condicion;iteracion)
+        for (int i = 0; i<=20; i++){
+            System.out.println(i);
+
+        }
+        System.out.println("-------------------------");
+        //conteo del 0 al 20 de 2 en 2
+        for (int i = 0; i<=20; i+=2){
+            System.out.println(i);
+        }
+        System.out.println("-------------------------");
+        //conteo descendente del 20 al 0
+        for (int i =20;i>=0 ;i--){
+            System.out.println(i);
+        }
+        System.out.println("-------------------------");
+        //conteo descendente del 20 al 0 de -5 en -5
+        for (int i =20;i>=0 ;i-=5){
+            System.out.println(i);
+        }
+
+    }
+}
