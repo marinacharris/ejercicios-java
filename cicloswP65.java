eea40b6ce4c38f0ecde89e1fb7b35e3bbf681a59
marinacharris <85807129+marinacharris@users.noreply.github.com>
2021-07-01 07:29:18 -0500

    Add files via upload

---
+++ cicloswP65.java
@@ -0,0 +1,16 @@
+public class cicloswP65 {
+    public static void main(String[] args) {
+        //conteo de 0 a 20;
+        int conta = 50;
+        while (conta<=20){
+            System.out.println(conta);
+            conta ++;
+        }
+        System.out.println("------------------------------------------------");
+        conta = 50;
+        do{
+            System.out.println(conta);
+            conta ++;
+        }while(conta<=20);
+    }
+}
