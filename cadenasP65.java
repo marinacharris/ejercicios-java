bf1995649b626fbc9285e346c1a02d81cf93c9ba
marinacharris <85807129+marinacharris@users.noreply.github.com>
2021-07-01 08:09:39 -0500

    Add files via upload

---
+++ cadenasP65.java
@@ -0,0 +1,12 @@
+public class cadenasP65 {
+    public static void main(String[] args) {
+        String mensaje = "       esto es un ejemplo       ";
+        System.out.println(mensaje.toUpperCase());
+        System.out.println(mensaje.toUpperCase().trim());
+        System.out.println(mensaje.length());
+
+        int numero = 20;
+        String cadena = String.valueOf(numero);
+
+    }
+}
